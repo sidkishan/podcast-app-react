bc11880dd98aa778ae8335b8f45c4a18901ac988
Siddharth Kishan <kishan.siddharth2939@gmail.com>
2023-12-30 13:32:21 +0530

    minor bug fix

--- src/components/CommonComponents/PrivateRoutes.js
+++ src/components/CommonComponents/PrivateRoutes.js
@@ -1,14 +1,19 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import Loader from "./Loader";
+import { toast } from "react-toastify";
 
 const PrivateRoutes = () => {
   const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
   if (loading) {
     return <Loader />;
   } else if (!user || error) {
-    //toast.error("Please login first!!!");
+    //navigate("/");
+    toast.error("Please login first!!!", {
+      toastId: "success1", //for avoiding multiple same error messages
+    });
     return <Navigate to="/" />;
   } else {
     return <Outlet />;
